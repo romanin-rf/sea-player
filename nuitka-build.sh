bc75f7b67afbe52f69e33d0568d6907d61fad48b
Romanin <60302782+romanin-rf@users.noreply.github.com>
2025-01-05 19:09:27 +0200

    Refactoring №34

---
+++ nuitka-build.sh
@@ -0,0 +1,10 @@
+nuitka \
+    --standalone --onefile --follow-imports --jobs=4 \
+    --windows-icon-from-ico="icons/icon.ico" --linux-icon="icons/icon_256x256.png" \
+    --company-name="Romanin" --product-name="SeaPlayer" \
+    --file-version="0.10.0.34" --product-version="0.10.0.34" \
+    --file-description="SeaPlayer is a player that works in the terminal." \
+    --output-dir="bin/nuitka" --output-filename="seaplayer" "./seplayer-main.py"
+cp -Rf "./seaplayer/assets" "./bin/nuitka/assets"
+cp -Rf "./seaplayer/style" "./bin/nuitka/style"
+cp -Rf "./seaplayer/langs" "./bin/nuitka/langs"
